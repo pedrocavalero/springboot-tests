be638415f9c13a70f5461c636e548ac5baff4477
pedro <pedro@pedrocavaleroacer>
2021-02-14 15:55:43 -0300

    Adicionando o Spring fox para gerar o swagger da nossa API

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class DemoApplication {
